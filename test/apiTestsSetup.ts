a200c0d269e12dcfe013dbb0f21fe02b4b5510eb
papitoulini <113286556+Papitoulini@users.noreply.github.com>
2025-02-25 20:40:20 +0200

    dummy

--- test/apiTestsSetup.ts
+++ test/apiTestsSetup.ts
@@ -18,3 +18,16 @@ export = async () => {
   }
   )
 }
+
+const papi = async () => {
+  // eslint-disable-next-line no-async-promise-executor,@typescript-eslint/no-misused-promises
+  await new Promise<void>(async (resolve, reject) => {
+    await server.start((err?: Error) => {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  }
+  )
+}
